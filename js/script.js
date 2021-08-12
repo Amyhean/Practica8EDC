729f36b90bca955f20d14defade8130fd89486ff
Amyhean <amyburzumburton@gmail.com>
2021-08-11 19:52:27 -0500

    mi primer commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Variables
+var producto = 'vestido'; // iniciar la variable y para darle un valor
+var disponible; // iniciamos la variable pero sin valor
+
+var disponible = true;
+var producto = true;
+
+var producto1 = 'zapatos',
+    disponible = true,
+    categoria = 'Ropa';
+
+var disponible = 'sandalias';  
+
+// estilos para las variables
+
+var nombre_producto = 'Moños Rosa'; //underscore
+var nombreProducto = 'Moños Rosa'; // Camelcase
+var NombreProducto ='Moños Rosa'; // Pascal case ( preferentemente se usa para hacer clases)
+var nombreproducto = 'Moños Rosa'; // se le conoce como lo normal o mas común en js
+
+console.log(producto); // Forma correcta de visualizar una variable en nuestra consola ( poner el nombre de la variable tal cual)
+console.log(PRODUCTO); // Forma incorrecta.
